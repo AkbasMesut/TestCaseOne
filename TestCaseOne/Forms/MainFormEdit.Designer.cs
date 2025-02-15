94f8008e78668f62c6d7408c2ba8a7db7c3f4d5e
mesut akbas <Akbas_Mesut@outlook.com>
2025-02-15 12:24:34 +0300

    15.02.2025 tarihli 1. değişiklik

--- TestCaseOne/Forms/MainFormEdit.Designer.cs
+++ TestCaseOne/Forms/MainFormEdit.Designer.cs
@@ -28,25 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(62, 53);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(485, 43);
-            this.listBox1.TabIndex = 0;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(62, 178);
+            this.button1.Location = new System.Drawing.Point(62, 408);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(135, 55);
             this.button1.TabIndex = 1;
@@ -55,7 +46,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(242, 178);
+            this.button2.Location = new System.Drawing.Point(242, 408);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(135, 55);
             this.button2.TabIndex = 1;
@@ -64,7 +55,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(412, 178);
+            this.button3.Location = new System.Drawing.Point(412, 408);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(135, 55);
             this.button3.TabIndex = 1;
@@ -78,17 +69,29 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Arial", 14.26415F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox1.Location = new System.Drawing.Point(62, 117);
+            this.textBox1.Location = new System.Drawing.Point(62, 325);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(485, 44);
+            this.textBox1.Size = new System.Drawing.Size(485, 42);
             this.textBox1.TabIndex = 1;
+   
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Arial Narrow", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 22;
+            this.listBox1.Location = new System.Drawing.Point(62, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(485, 246);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // MainFormEdit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(605, 284);
+            this.ClientSize = new System.Drawing.Size(598, 512);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -103,11 +106,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
--- TestCaseOne/Forms/MainFormEdit.cs
+++ TestCaseOne/Forms/MainFormEdit.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,16 +13,112 @@ namespace TestCaseOne.Forms
 {
     public partial class MainFormEdit : Form
     {
-        public MainFormEdit(List<string> featureNames)
+        private string connectionString = "Data Source=veritabani.db;Version=3;";
+
+        public MainFormEdit(List<string> featureNames) // Tek parametreli constructor
         {
             InitializeComponent();
             listBox1.Items.AddRange(featureNames.ToArray());
         }
 
+        private void MainFormEdit_Load(object sender, EventArgs e)
+        {
+            ListeyiYukle();
+        }
+
+        // Verileri ListBox'a Yükle
+        private void ListeyiYukle()
+        {
+            listBox1.Items.Clear();
+            using (SQLiteConnection con = new SQLiteConnection(connectionString))
+            {
+                con.Open();
+                SQLiteCommand cmd = new SQLiteCommand("SELECT FeatureName FROM MainFeature", con);
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    listBox1.Items.Add(reader["FeatureName"].ToString());
+                }
+            }
+        }
+
+        // ListBox'tan seçim yapıldığında TextBox'a getir
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string selectedFeature = listBox1.SelectedItem.ToString();
-            MessageBox.Show($"Seçilen Özellik: {selectedFeature}");
+            if (listBox1.SelectedItem != null)
+            {
+                textBox1.Text = listBox1.SelectedItem.ToString();
+            }
+        }
+
+        // Yeni Özellik Ekle
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                using (SQLiteConnection con = new SQLiteConnection(connectionString))
+                {
+                    con.Open();
+                    SQLiteCommand cmd = new SQLiteCommand("INSERT INTO MainFeature (FeatureName) VALUES (@name)", con);
+                    cmd.Parameters.AddWithValue("@name", textBox1.Text);
+                    cmd.ExecuteNonQuery();
+                }
+                ListeyiYukle();
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Özellik adı boş olamaz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        // Seçili Özelliği Güncelle
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null && !string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                string eskiOzellik = listBox1.SelectedItem.ToString();
+                using (SQLiteConnection con = new SQLiteConnection(connectionString))
+                {
+                    con.Open();
+                    SQLiteCommand cmd = new SQLiteCommand("UPDATE MainFeature SET FeatureName = @newName WHERE FeatureName = @oldName", con);
+                    cmd.Parameters.AddWithValue("@newName", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@oldName", eskiOzellik);
+                    cmd.ExecuteNonQuery();
+                }
+                ListeyiYukle();
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir özellik seçin ve yeni adı girin!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        // Seçili Özelliği Sil
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string seciliOzellik = listBox1.SelectedItem.ToString();
+                DialogResult result = MessageBox.Show("Bu özelliği silmek istediğinize emin misiniz?", "Silme Onayı", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    using (SQLiteConnection con = new SQLiteConnection(connectionString))
+                    {
+                        con.Open();
+                        SQLiteCommand cmd = new SQLiteCommand("DELETE FROM MainFeature WHERE FeatureName = @name", con);
+                        cmd.Parameters.AddWithValue("@name", seciliOzellik);
+                        cmd.ExecuteNonQuery();
+                    }
+                    ListeyiYukle();
+                    textBox1.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen silinecek bir öğe seçin!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
